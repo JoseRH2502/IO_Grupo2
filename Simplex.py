04ed5f6f090eb1e20c8a52758aa1535c950da711
johanec <steven_2110@outlook.com>
2022-09-24 09:38:23 -0600

    actualizacion

--- Simplex.py
+++ Simplex.py
@@ -5,9 +5,15 @@ from SimplexTabular import *
 
 def main():
 
-    archivo = sys.argv[1] # al ejecutar en terminal " python3 Simplex.py -h **.txt " 
+    archivo = sys.argv[1] # recibe archivo al ejecutar en terminal " python3 Simplex.py -h **.txt " 
     problema = leerTxt("Problemas/" + archivo)
-    simplexTabular(problema)
+
+    if (problema[0][0] == 1):# valida si el problema requiere ser resuelto por el metodo simplexTabular
+      simplexTabular(problema)
+    
+    elif(problema[0][0] == 2):# valida si el problema requiere ser resuelto por el metodo simplexDosFases
+        print("metodo dos fases aun no esta implementado")
+    
    
 main()
 
