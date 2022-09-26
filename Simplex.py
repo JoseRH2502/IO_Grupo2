aa0d48398a4044a544e4e0bab57c169211f90d74
JoseRH25 <jlgamer25.ah@gmail.com>
2022-09-25 18:48:24 -0600

    implemetación del escritor

--- Simplex.py
+++ Simplex.py
@@ -2,19 +2,21 @@ import sys
 from Lector import *
 from SimplexTabular import *
 from SimplexDosFases import*
+from Escritor import *
 
 
 def main():
 
     archivo = sys.argv[1] # recibe archivo al ejecutar en terminal " python3 Simplex.py -h **.txt " 
-    #archivo = "p_min_1.txt"
+    #archivo = "p_min_2.txt"
     problema = leerTxt("Problemas/" + archivo)
 
     if (problema[0][0] == 1):# valida si el problema requiere ser resuelto por el metodo simplexTabular
         simplexTabular(problema)
     
     elif(problema[0][0] == 2):# valida si el problema requiere ser resuelto por el metodo simplexDosFases
-        dosFases(problema)
+        escribir("Solución",  dosFases(problema))
+
      
    
 main()
